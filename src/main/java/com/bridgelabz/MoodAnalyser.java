fabb6dddbbd061b24c1366be6696616d04256fd6
Janani <vsvprema@gmail.com>
2021-11-22 17:16:13 +0530

    Added:Null mood Should Return Happy

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -2,15 +2,23 @@ package com.bridgelabz;
 
 public class MoodAnalyser {
     private String message;
-    public MoodAnalyser(){
-        this.message=message;
+
+    public MoodAnalyser() {
+        this.message = message;
     }
-    public MoodAnalyser(String message){
-        this.message=message;
+
+    public MoodAnalyser(String message) {
+        this.message = message;
     }
-    public String analyseMood(){
-            if(message.contains("Sad"))
+
+    public String analyseMood() {
+        try {
+            if (message.contains("Sad"))
                 return "SAD";
-                return "SAD";
+            else
+                return "HAPPY";
+        }catch (NullPointerException e){
+            return "HAPPY";
+        }
     }
 }
